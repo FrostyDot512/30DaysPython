d2dafea61db3c6bb968647e4920a38b502e437eb
FrostyDot67 <anwaarnbatala@gmail.com>
2026-01-22 13:12:32 +0300

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,34 @@
+#Level 2
+print(3 + 4)
+print(3 - 1)
+print(3 * 4)
+print(3 / 4)
+print(3 % 4)
+print(3 // 4)
+print(3 ** 4)
+
+print("Anwaar Batala")
+print("Batala")
+print("Uganda")
+print("I am enjoying 30 days Python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(["Anwaar", "Uganda", "Congo"]))
+print(type("Uganda"))
+
+#Level 3
+print(type(35))
+print(type(4.2))
+print(type(3+2j))
+print(type("Noah"))
+print(type(True))
+print(type([34, 13, 53]))
+print(type((34, 134, 210)))
+print(type({23, 53, 1340}))
+print(type({"name": "Anwaar"}))
+
+# Euclidean distance using (2,3) and (10,8)
+
+print(((10-2)**2 + (8-3)**2)**0.5)
