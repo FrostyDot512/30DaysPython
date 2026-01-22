5c5a3d09a7321798117381a683e07bd64ff0a58a
FrostyDot67 <anwaarnbatala@gmail.com>
2026-01-22 21:09:05 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,55 @@
+#Day 2: 30 Days of python programming
+ 
+#Level 1
+first_name = "Frosty" #1
+last_name = "Dot" #2
+full_name = first_name + last_name #3
+country = "South Africa" #4
+city_name = "Cape Town" #5
+age = 18 #6
+year = 2026 #7
+is_married = False #8
+is_true = True #9
+is_light_on = True #10
+x, y, z = 1, 2, 3 #11
+
+#Level 2
+#1
+type(first_name) 
+type(last_name)
+type(full_name)
+type(country)
+type(city_name)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
+
+len(first_name) #2
+max(len(first_name), len(last_name)) #3
+#4
+num_one = 5
+num_two = 4
+total_sum = num_one + num_two #5
+subtract = num_two - num_one #6
+multiply = num_two * num_one #7
+divide = num_one / num_two #8
+mod = num_two % num_one #9
+exponential = num_one ** num_two #10
+floor_division = num_one // num_two #11
+#12
+circle_radius = int(input("Enter your radius: "))
+pi = 3.14159265359
+circle_area = pi * (circle_radius**2)
+circle_circumfrence = 2 * pi * circle_radius
+#13
+First_Name1 = input("Enter your first name: ")
+Last_Name1 = input("Enter your last name: ")
+Country1 = input("Enter your country: ")
+Age1 = input("Enter your age: ")
+help('keywords') #14
+
+
+
+
