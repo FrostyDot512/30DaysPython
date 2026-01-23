bce1c49668e4a6e3f0512fcb41297dd122153a5d
FrostyDot67 <anwaarnbatala@gmail.com>
2026-01-24 00:32:28 +0300

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,31 @@
+my_age = 18 #1
+my_height = 186.0 #2
+complex_number = 1 + 3j #3
+#4
+base = int(input("Enter triangle base: "))
+height = int(input("Enter triangle height: "))
+triangle_area = (0.5 * base * height)
+#5
+side_a = int(input("Enter side a: "))
+side_b = int(input("Enter side b: "))
+side_c = int(input("Enter side c: "))
+triangle_perimeter = side_a + side_b + side_c
+#6
+length = int(input("Enter the legnth: "))
+width = int(input("Enter the width: "))
+rectangle_area = length * width
+rectangle_perimeter = 2 *(length + width)
+#7
+pi = 3.14
+radius = int(input("Enter radius: "))
+circle_area = pi * (radius **2)
+circle_circumference = 2 * pi * radius
+#8
+#work on it laterrrr
+#9
+x1, y1 = 2, 6
+x2, y2 = 6, 10
+print(slope = ((10-2) / (6-2)))
+print(distance = (((x2 - x1)**2 + (y2 - y1)**2)**0.5))
+
+
