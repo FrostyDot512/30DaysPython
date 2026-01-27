248123902f018c43c7efa72af249fe72718c9d32
FrostyDot67 <anwaarnbatala@gmail.com>
2026-01-27 23:18:52 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,28 @@
+print("{} {} {} {}".format("Thirty", "Days", "Of", "Python") ) #1
+print("{} {} {}".format("Coding", "For", "All") ) #2
+Company = "Coding For All" #3
+print(Company) #4
+print(len(Company)) #5
+print(Company.upper()) #6
+print(Company.lower()) #7
+#8
+print(Company.capitalize())
+print(Company.title())
+print(Company.swapcase())
+print(Company.strip("Coding")) #9
+#10
+print(Company.index("Coding")) 
+print(Company.find("Coding"))
+print(Company.replace("Coding", "Python")) #11
+#12
+Python_Variable = "Python for Everyone"
+print(Python_Variable.replace("Everyone", "All"))
+#13
+
+
+
+
+
+
+
+
